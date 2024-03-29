8353e8b85a138e3786067f14275cb0600eb7ed52
DhavitTheKing <165244479+DhavitTheKing@users.noreply.github.com>
2024-03-29 17:44:48 +0530

    Update Perimeter and Area Finder.py

--- Perimeter and Area Finder.py
+++ Perimeter and Area Finder.py
@@ -1,10 +1,20 @@
 unknown=input("What do you need to find? (Perimeter or Area)")
 if unknown=="Perimeter":
-    unknown_2=input("Which shape's perimeter do you need to find? (Square or Rectangle)")
+    unknown_2=input("Which shape's perimeter/circumfrence do you need to find? (Square, Circle, Triangle or Rectangle)")
     if unknown_2=="Square":
         side=float(input("Enter your side length:"))
         s_perimeter=side*4
         print("The perimeter of the square is ",s_perimeter)
+    elif unknown_2=="Circle":
+        radius=float(input("Enter the radius of the circle:"))
+        circ=(3.14*2)*radius
+        print("The circumfrence of the circle is ",circ)
+    elif unknown_2=="Triangle":
+        s1=float(input("Enter the length of the first side:"))
+        s2=float(input("Enter the length of the second side:"))
+        s3=float(input("Enter the length of the third side:"))
+        peri=s1+s2+s3
+        print("The perimeter of the triangle is ",peri)
     else:
         length=float(input("Enter your length:"))
         breadth=float(input("Enter your breadth:"))
