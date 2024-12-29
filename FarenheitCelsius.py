b6a2bbd850f5a8962e066f7a1cf5baaf4bf70143
DhavitTheKing <165244479+DhavitTheKing@users.noreply.github.com>
2024-12-29 10:06:20 +0530

    Update FarenheitCelsius.py

--- FarenheitCelsius.py
+++ FarenheitCelsius.py
@@ -1,6 +1,6 @@
 unknown=input("What do you need to convert into?(Farenheit or Celsius)")
 if unknown=="Celsius":
-    f=int(input("Enter the temperature in F°:"))
+    f=int(input("Enter the temperature in °F:"))
     c=(f-32)*5/9
     print("The temperature in °C is ",c)
 else:
